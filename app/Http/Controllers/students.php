75e1cf54c197ea124cb4da79403a9083590809e3
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-04-28 21:34:55 +0000

    create a stub

---
+++ app/Http/Controllers/students.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class students extends Controller
+{
+    //
+    public function index()
+    {
+        
+        return "Hello from index";
+    }
+}
