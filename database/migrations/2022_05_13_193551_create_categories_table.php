2cc51fd583a39503ea8552140cb7b30832ec37c5
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-13 19:56:45 +0000

    update migration

---
+++ database/migrations/2022_05_13_193551_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('content');
+            $table->string('image');
+            $table->timestamps();
+
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Category::class);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
