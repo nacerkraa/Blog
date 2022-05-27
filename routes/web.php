0d6f24bc8248e9605c07793bee4c8014e5f8d026
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-27 09:37:59 +0000

    This is a new comment

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+| This is a new comment
 */
 
 
