b0f5551167b2a7825495527b91dbef77fe045da8
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-28 23:59:42 +0000

    create route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,5 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', [UserController::class, 'index'])->name('user.index');
 
 
