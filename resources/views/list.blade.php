a2baab53ebf6df44ee8b3df954543e3039fb93b2
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-29 02:06:17 +0000

    create a list of articles

---
+++ resources/views/list.blade.php
@@ -0,0 +1,1 @@
+<h2>This is a list of <article>articles</article></h2>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,6 @@ Route::get('/', function () {
 });
 
 Route::get('home', [UserController::class, 'index'])->name('user.index');
+Route::get('articles', [ArticleController::class, 'index'])->name('article.index');
 
 
