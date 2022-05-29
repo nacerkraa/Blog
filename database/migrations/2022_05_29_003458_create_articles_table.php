ec204e1d4895105c6545011f076507b17c62160c
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-29 00:56:01 +0000

    add a new filed

--- database/migrations/2022_05_29_003458_create_articles_table.php
+++ database/migrations/2022_05_29_003458_create_articles_table.php
@@ -17,6 +17,7 @@ class CreateArticlesTable extends Migration
             $table->id();
             $table->string('title');
             $table->string('content');
+            $table->string('category');
             $table->timestamps();
         });
     }
