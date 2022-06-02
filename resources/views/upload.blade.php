25f0c8a0f8ed578ddc3daf556320353e42a8e5fb
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-06-02 18:10:16 +0000

    update

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -3,5 +3,5 @@
 <form action="" method="post" enctype="multipart/form-data">
     @csrf
     <input type="file" name="file" > <br><br>
-    <button type="submit">Upload file</button>
+    <button type="submit">Upload file1</button>
 </form>
