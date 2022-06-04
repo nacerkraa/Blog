9b9dc274a88af5928fad21a0829ef99813baeed6
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-06-04 11:42:49 +0000

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <h3>
     @if(session()->has('user'))
         <div>Hello your name is {{session("user")}}</div>
-        <a href="/lougout">Lougout</a>
+        <a href="/lougout">Logout</a>
     @else 
         <div>There is no user yet</div>
 
