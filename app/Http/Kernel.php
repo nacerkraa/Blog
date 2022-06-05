3eee80622bb9d8ada678cd849d99dd985d3d826c
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-06-05 19:43:24 +0000

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,7 +48,6 @@ class Kernel extends HttpKernel
 
     /**
      * The application's route middleware.
-     *
      * These middleware may be assigned to groups or used individually.
      *
      * @var array<string, class-string|string>
