e2a485b900c82d7ff6cc8c1741015a6cd5b7d834
Bayron Vazquez <bayronnjonathan@gmail.com>
2022-06-08 15:52:44 -0500

    jQuery installation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+/**
+ * Library: jQuery
+ * Offical WEBSite: https://jquery.com/
+ * the following line of code imports the jquery library in all global application of laravel,
+ * it was installed with the npm install command (npm i jquery).
+ */
+import jquery from 'jquery';
+window.$ = window.jQuery = jquery;
